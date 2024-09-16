770a8b21847a5fc37c4eeab54d9b6a59e0ed158c
caotianhao <1981471780@qq.com>
2024-09-17 00:00:20 +0800

    feat: update ks oj three files

--- 202312/OJ/jinshan2.go
+++ 202312/OJ/jinshan2.go
@@ -9,31 +9,17 @@ func maxFruitValue(apple, peach, fruitA, fruitB int) int {
 	// apple + apple + peach = fruitA
 	// apple + peach + peach = fruitB
 	value := math.MinInt64
-	maxA, maxB := minFruit(apple/2, peach), minFruit(peach/2, apple)
+	maxA, maxB := min(apple/2, peach), min(peach/2, apple)
 	for i := 0; i <= maxA; i++ {
-		value = maxValue(value, i*fruitA+minFruit((peach-i)/2, apple-2*i)*fruitB)
+		value = max(value, i*fruitA+min((peach-i)/2, apple-2*i)*fruitB)
 	}
 	for i := 0; i <= maxB; i++ {
-		value = maxValue(value, i*fruitB+minFruit((apple-i)/2, peach-2*i)*fruitA)
+		value = max(value, i*fruitB+min((apple-i)/2, peach-2*i)*fruitA)
 	}
 	if value == math.MinInt64 {
 		return 0
 	}
 	return value
-}
-
-func minFruit(a, b int) int {
-	if a > b {
-		return b
-	}
-	return a
-}
-
-func maxValue(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
 
 func main() {
