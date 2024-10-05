e4ed48bd8cfd029df9a42a9a3295936af85b2243
caotianhao <1981471780@qq.com>
2024-10-05 23:01:16 +0800

    moji: new moji with two attacks

--- 202410/moji.go
+++ 202410/moji.go
@@ -6,29 +6,29 @@ import (
 
 func main() {
 	// 缺数据，做不了，回归分析也不会，缺个 25 和 33 的数据
-	moji := make([]int, 36)
-	moji[10] = 1705
-	moji[11] = 1925
-	moji[12] = 2200
-	moji[13] = 2530
-	moji[14] = 2860
-	moji[15] = 3245
-	moji[16] = 3630
-	moji[17] = 4070
-	moji[18] = 4510
-	moji[19] = 5005
-	moji[20] = 5500
-	moji[21] = 5885
-	moji[22] = 6875
-	moji[23] = 7975
-	moji[27] = 13970
-	moji[30] = 20570
-	moji[31] = 23155
-	moji[35] = 34760
+	additionalAttack := make([]int, 36)
+	additionalAttack[10] = 1705
+	additionalAttack[11] = 1925
+	additionalAttack[12] = 2200
+	additionalAttack[13] = 2530
+	additionalAttack[14] = 2860
+	additionalAttack[15] = 3245
+	additionalAttack[16] = 3630
+	additionalAttack[17] = 4070
+	additionalAttack[18] = 4510
+	additionalAttack[19] = 5005
+	additionalAttack[20] = 5500
+	additionalAttack[21] = 5885
+	additionalAttack[22] = 6875
+	additionalAttack[23] = 7975
+	additionalAttack[27] = 13970
+	additionalAttack[30] = 20570
+	additionalAttack[31] = 23155
+	additionalAttack[35] = 34760
 
 	for i := 0; i < 35; i++ {
-		if moji[i] != 0 && moji[i+1] != 0 {
-			fmt.Printf("%d - %d = %d\n", i+1, i, moji[i+1]-moji[i])
+		if additionalAttack[i] != 0 && additionalAttack[i+1] != 0 {
+			fmt.Printf("%d - %d = %d\n", i+1, i, additionalAttack[i+1]-additionalAttack[i])
 		}
 	}
 
