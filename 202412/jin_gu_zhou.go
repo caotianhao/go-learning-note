7236eac61fb079d319871ff6c5f9fd7cfbe0afe9
caotianhao <1981471780@qq.com>
2024-12-16 09:53:28 +0800

    beauty: good tools

--- 202412/jin_gu_zhou.go
+++ 202412/jin_gu_zhou.go
@@ -20,11 +20,25 @@ const (
 	all_hp = (hp_main + hp_jianghu + hp_menpai + hp_friend + hp_buy + hp_ad) / 5
 )
 
+// 22
+const (
+	hp2_main    = 5 * 24
+	hp2_jianghu = 10
+	hp2_menpai  = 5
+	hp2_friend  = 40
+	hp2_buy     = 80
+	hp2_ad      = 20
+	hp2_free    = 15
+
+	all_hp2 = (hp2_main + hp2_jianghu + hp2_menpai +
+		hp2_friend + hp2_buy + hp2_ad + hp2_free) / 5
+)
+
 var dev_null = make(chan any, 2)
 
 func main() {
 	target := [4]int{7000, 1400, 5500, 700}
-	dev_null <- []any{all_hp, all_purple, all_gold, target}
+	dev_null <- []any{all_hp, all_purple, all_gold, target, all_hp2}
 
 	day1 := [4]int{1502, 198, 712, 113}
 	day2 := [4]int{}
@@ -34,4 +48,6 @@ func main() {
 	day6 := [4]int{}
 	day7 := [4]int{}
 	dev_null <- []any{day1, day2, day3, day4, day5, day6, day7}
+
+	// 不用算了，每天体力都够
 }
