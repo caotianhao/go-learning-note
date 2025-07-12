588c205075484e970bc822ebe10b0d90d9069a6a
caotianhao <1981471780@qq.com>
2025-07-12 20:39:34 +0800

    test

--- 202507/seer_restraint.go
+++ 202507/seer_restraint.go
@@ -33,35 +33,78 @@ func GetSeerElementRestraint1v2(atk, def1, def2 int) float64 {
 	ab := GetSeerElementRestraint1v1(atk, def1)
 	ac := GetSeerElementRestraint1v1(atk, def2)
 
-	return (ab + ac) / 2
+	return singleCalculate(ab, ac)
 }
 
 func GetSeerElementRestraint2v1(atk1, atk2, def int) float64 {
 	ac := GetSeerElementRestraint1v1(atk1, def)
 	bc := GetSeerElementRestraint1v1(atk2, def)
 
-	var div float64 = 2
-	if ac == model.SeerElementRestraintResultRestraint && bc == model.SeerElementRestraintResultRestraint {
-		div = 4
-	}
-	if ac == model.SeerElementRestraintResultZero || bc == model.SeerElementRestraintResultZero {
-		div = 4
-	}
-
-	return (ac + bc) / div
+	return singleCalculate(ac, bc)
 }
 
 func GetSeerElementRestraint2v2(atk1, atk2, def1, def2 int) float64 {
 	abc := GetSeerElementRestraint2v1(atk1, atk2, def1)
 	abd := GetSeerElementRestraint2v1(atk1, atk2, def2)
-	return (abc + abd) / 2
+
+	res := (abc + abd) / 2
+	if res > 4 {
+		return 4
+	}
+	return res
+}
+
+func singleCalculate(res1, res2 float64) float64 {
+	var div float64 = 2
+	if res1 == model.SeerElementRestraintResultRestraint && res2 == model.SeerElementRestraintResultRestraint {
+		div = 1
+	}
+	if res1 == model.SeerElementRestraintResultZero || res2 == model.SeerElementRestraintResultZero {
+		div = 4
+	}
+
+	return (res1 + res2) / div
 }
 
 func main() {
-	result1v1 := GetSeerElementRestraint1v1(model.SeerElementTypeFire, model.SeerElementTypeGrass)
-	result1v2 := GetSeerElementRestraint1v2(model.SeerElementTypeGod, model.SeerElementTypeDark, model.SeerElementTypeEvil)
-	result2v1 := GetSeerElementRestraint2v1(model.SeerElementTypeFly, model.SeerElementTypeSuper, model.SeerElementTypeWar)
-	result2v2 := GetSeerElementRestraint2v2(model.SeerElementTypeMachine, model.SeerElementTypeSuper, model.SeerElementTypeElectricity, model.SeerElementTypeIce)
-
-	fmt.Println(result1v1, result1v2, result2v1, result2v2)
+	fmt.Println(GetSeerElementRestraint2v2(
+		model.SeerElementTypeHoly, model.SeerElementTypeNature,
+		model.SeerElementTypeElectricity, model.SeerElementTypeFire,
+	) == 4)
+	fmt.Println(GetSeerElementRestraint2v2(
+		model.SeerElementTypeElectricity, model.SeerElementTypeFire,
+		model.SeerElementTypeHoly, model.SeerElementTypeNature,
+	) == 0.5)
+	fmt.Println(GetSeerElementRestraint2v2(
+		model.SeerElementTypeChaos, model.SeerElementTypeDragon,
+		model.SeerElementTypeIce, model.SeerElementTypeSuper,
+	) == 2.5)
+	fmt.Println(GetSeerElementRestraint2v2(
+		model.SeerElementTypeIce, model.SeerElementTypeSuper,
+		model.SeerElementTypeChaos, model.SeerElementTypeDragon,
+	) == 0.875)
+	fmt.Println(GetSeerElementRestraint1v2(
+		model.SeerElementTypeDimension,
+		model.SeerElementTypeFly, model.SeerElementTypeSuper,
+	) == 4)
+	fmt.Println(GetSeerElementRestraint2v1(
+		model.SeerElementTypeFly, model.SeerElementTypeSuper,
+		model.SeerElementTypeDimension,
+	) == 0.75)
+	fmt.Println(GetSeerElementRestraint2v2(
+		model.SeerElementTypeHoly, model.SeerElementTypeFly,
+		model.SeerElementTypeGround, model.SeerElementTypeMystery,
+	) == 0.875)
+	fmt.Println(GetSeerElementRestraint2v2(
+		model.SeerElementTypeGround, model.SeerElementTypeMystery,
+		model.SeerElementTypeHoly, model.SeerElementTypeFly,
+	) == 0.75)
+	fmt.Println(GetSeerElementRestraint1v2(
+		model.SeerElementTypeGrass,
+		model.SeerElementTypeLight, model.SeerElementTypeMystery,
+	) == 1.5)
+	fmt.Println(GetSeerElementRestraint2v1(
+		model.SeerElementTypeLight, model.SeerElementTypeMystery,
+		model.SeerElementTypeGrass,
+	) == 0.25)
 }
