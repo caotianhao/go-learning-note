2a72af0771ea812aa273a1a8ebf11cb98431904b
caotianhao <1981471780@qq.com>
2025-08-14 00:08:28 +0800

    up 31-1

--- 202508/moji.go
+++ 202508/moji.go
@@ -6,19 +6,19 @@ import (
 )
 
 const (
-	expSucceed      = 15408
-	expFail         = 14338
+	expSucceed      = 15702
+	expFail         = 15408
 	expUpgradePerLv = 1300000
 
 	lvMax = 449
 	lvNow = 344
 
-	hp_ad_normal_double = -25
-	hp_ad_hard_double   = -25
-	hp_friend           = 40
-	hp_login_60min      = 10
-	hp_leader           = 5
-	hp_ad               = 40
+	hpAdNormalDouble = -25
+	hpAdHardDouble   = -25
+	hpFriend         = 40
+	hpLogin60min     = 10
+	hpLeader         = 5
+	hpAd             = 40
 
 	ironBreak = 190000
 	ironNow   = 77852
@@ -28,9 +28,9 @@ const (
 	exp0to9    = 10 * expSucceed
 	exp9to24   = (15 * 60 / 45) * expSucceed
 	expJianghu = 4000*4 + 5000*3 + 6000*3
-	expDouble  = (hp_ad_hard_double + hp_ad_normal_double) / -5 * expFail
-	expOther   = (hp_leader + hp_ad + hp_login_60min + hp_friend +
-		hp_ad_hard_double + hp_ad_normal_double) / 5 * expSucceed
+	expDouble  = (hpAdHardDouble + hpAdNormalDouble) / -5 * expFail
+	expOther   = (hpLeader + hpAd + hpLogin60min + hpFriend +
+		hpAdHardDouble + hpAdNormalDouble) / 5 * expSucceed
 
 	expPerDay = exp0to9 + exp9to24 + expJianghu + expDouble + expOther
 )
