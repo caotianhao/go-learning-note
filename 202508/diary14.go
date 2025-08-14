5f20b166280b91836808f3417d1d12093bec244e
caotianhao <1981471780@qq.com>
2025-08-14 22:41:17 +0800

    diary0814

---
+++ 202508/diary14.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	println("요전 날, 내 친구가 내가 왜 멍해 있었냐고 물었다")
+	println("나는 단지 생각하고 있다고 말했다")
+	println("내가 뭐야, 나쁜 사람")
+	println("내가 비양심적인 짓을 한 건 아니겠지")
+	println("내 친구는 나를 위로하고 안된다고 말했습니다")
+	println("친구에게 좋고, 관심 있는 사람들에게 좋습니다")
+	println("항상 당직")
+	println("그래서 제가 말했죠")
+	println("글쎄, 왜")
+	println("나는 항상 길을 잃는 사람이다...")
+}
