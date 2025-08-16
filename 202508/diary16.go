f4e695389cc14a10af45a2999560da33a8f5f831
caotianhao <1981471780@qq.com>
2025-08-16 23:52:11 +0800

    250816

---
+++ 202508/diary16.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("달빛 속의 독백은 호박색으로 굳어졌습니다")
+	fmt.Println("마침내 사과 없이 사랑받는 법을 배웠을 때")
+	fmt.Println("최선을 다하는 사람들")
+	fmt.Println("끈질긴 사람들을 위한 1인 쇼일 뿐입니다")
+	fmt.Println("시간은 뜨거운 손바닥 자국에서 진실을 추출합니다")
+	fmt.Println("사랑의 무게는 동등한 포옹으로 교환된 적이 없습니다")
+	fmt.Println("늦은 각성을 목격하기 위해 밤바람을 남겨두십시오")
+}
