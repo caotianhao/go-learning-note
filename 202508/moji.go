7ae4b89d70d10370bb7a221fa88816c117565f9f
caotianhao <1981471780@qq.com>
2025-09-04 21:23:18 +0800

    moji: drop shoe.lv===weapon.lv

--- 202508/moji.go
+++ 202508/moji.go
@@ -21,8 +21,9 @@ const (
 	hpLeader         = 5
 	hpAd             = 40
 
-	ironBreak = 193328
-	ironNow   = 157362
+	ironBreakWeapon = 83328
+	ironBreakShoe   = 110000
+	ironNow         = 157362
 
 	equipLv = 100
 
@@ -42,17 +43,21 @@ const (
 	expNatural = exp0to9 + exp9to24
 	expDouble  = expDoubleNormal + expDoubleHard
 	expEx      = expJianghu + expOther
+	expPerDay  = expNatural + expDouble + expEx
 
-	expPerDay = expNatural + expDouble + expEx
+	ironBreak = ironBreakShoe + ironBreakWeapon
 )
 
-func main() {
-	cuiFromGold2MaxSingle := make([]int, equipLv)
+var cuiFromGold2MaxSingle = make([]int, equipLv)
+
+func initCuiSlice() {
 	cuiFromGold2MaxSingle[35] = cui35
 	for i := 36; i < equipLv; i++ {
 		cuiFromGold2MaxSingle[i] += (i-35)*cuiDiff + cuiFromGold2MaxSingle[35]
 	}
+}
 
+func wsSame() {
 	startDate := time.Now()
 
 	for days := 1; ; days++ {
@@ -72,4 +77,35 @@ func main() {
 
 		fmt.Printf("%s, lv.%d, need %d iron\n", dateStr, curLv, needIron-curIron)
 	}
+}
+
+func wsUnSame() {
+	startDate := time.Now()
+
+	for days := 1; ; days++ {
+		curLv := lvNow + (days*expPerDay)/expUpgradePerLv
+		if curLv > lvMax {
+			break
+		}
+		tmp := curLv/10 + 1
+		if tmp > equipLv-1 {
+			break
+		}
+
+		ironPerDay := (tmp + 1) * 100
+		curIronW := ironNow + ironBreakWeapon + days*ironPerDay
+		curIronS := ironNow + ironBreakShoe + days*ironPerDay
+		needIron := cuiFromGold2MaxSingle[tmp]
+		dateStr := startDate.AddDate(0, 0, days).Format("2006-01-02")
+
+		fmt.Printf("%s, lv.%d, W %d iron, S %d iron\n",
+			dateStr, curLv, needIron-curIronW, needIron-curIronS)
+	}
+}
+
+func main() {
+	initCuiSlice()
+	wsSame()
+	fmt.Println("********************************************************")
+	wsUnSame()
 }
